6e77002ff244bff5e78a2b559677063175bdff86
matheushmendes26 <matheush.1998@hotmail.com>
2021-06-27 00:35:31 -0300

    finalizando projeto

--- src/route.js
+++ src/route.js
@@ -6,16 +6,19 @@ const route = express.Router()
 
 
 
-route.get('/create-pass',(req,res)=> res.render("index", {page: 'create-pass'}))
 route.get('/',(req,res)=> res.render("index",{page:'enter-room'}))
+route.get('/create-pass',(req,res)=> res.render("index", {page: 'create-pass'}))
 
 
-route.get('/room/:room',(req,res)=> res.render("room"))
+route.post('/create-room',RoomController.create)
+route.get('/room/:room',RoomController.open)
+route.post('/enterroom',RoomController.enter)
+
 
 //Formato de dentro do modal que tem que passar info
 route.post('/question/:room/:question/:action', QuestionController.index)  
 
-route.post('/create-room', RoomController.create)
+route.post('/create-room/:room', QuestionController.create)
 
 module.exports = route
 
